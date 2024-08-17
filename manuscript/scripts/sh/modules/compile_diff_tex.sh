3cefb0a0dae85ad8b1d2ea8b4347456cba422463
Yusuke Watanabe <wyusuuke@gmail.com>
2024-08-17 17:23:09 +1000

    .

--- manuscript/scripts/sh/modules/compile_diff_tex.sh
+++ manuscript/scripts/sh/modules/compile_diff_tex.sh
@@ -3,20 +3,21 @@
 echo -e "$0 ..."
 
 function compile_diff_tex() {
-    output_diff_tex=./main/diff.tex
+    input_diff_tex=./main/diff.tex
+    output_diff_pdf=./main/diff.pdf
 
-    if [ -s $output_diff_tex ]; then
-        echo -e "\nCompiling $output_diff_tex..."
-        yes '' | pdflatex -shell-escape $output_diff_tex >/dev/null
-        bibtex ${output_diff_tex%.tex} >/dev/null 2>&1
-        yes '' | pdflatex -shell-escape $output_diff_tex >/dev/null
-        yes '' | pdflatex -shell-escape $output_diff_tex >/dev/null
+    if [ -s $input_diff_tex ]; then
+        echo -e "\nCompiling $input_diff_tex..."
+        yes '' | pdflatex -shell-escape $input_diff_tex >/dev/null
+        bibtex ${input_diff_tex%.tex} >/dev/null 2>&1
+        yes '' | pdflatex -shell-escape $input_diff_tex >/dev/null
+        yes '' | pdflatex -shell-escape $input_diff_tex >/dev/null
 
-        if [ -f ./diff.pdf ]; then
-            echo -e "\n\033[1;33mCompiled: $output_diff_tex\033[0m"
+        if [ -f $output_diff_pdf ]; then
+            echo -e "\n\033[1;33mCompiled: $output_diff_pdf\033[0m"
         fi
     else
-        echo -e "\n$output_diff_tex is empty. Skip compiling $output_diff_tex"
+        echo -e "\n$input_diff_tex is empty. Skip compiling $input_diff_tex"
     fi
 }
 
